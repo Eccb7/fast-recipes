ab37b7c57aac712fba6d8704a44e8c9f8d24cf58
Briton Otieno <ojwangbriton@gmail.com>
2024-02-08 18:39:55 +0300

    update the foods model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,12 @@
 class Food < ApplicationRecord
-  has_and_belongs_to_many :inventories
+  has_many :inventory_foods
+  has_many :inventories, through: :inventory_foods
+
+  attribute :name, :string
+  attribute :measurement_unit, :string
+  attribute :price, :decimal
+
+  validates :name, presence: true, uniqueness: true
+  validates :measurement_unit, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end

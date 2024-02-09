ec595bd8b3076e9935afb82d5184dea65a5aa53e
Briton Otieno <ojwangbriton@gmail.com>
2024-02-09 23:10:59 +0300

    Add routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "rails/health#show"
+  root "recipes#public_recipes"
 
   resources :inventories, only: [:index, :show] do
     resources :inventory_foods, only: [:create, :destroy]
@@ -23,7 +18,7 @@ Rails.application.routes.draw do
     member do
       post 'toggle_public'
     end
-    resources :recipe_foods
+    resources :recipe_foods, only: [:new, :create, :destroy]
   end
 
   get 'public_recipes', to: 'recipes#public_recipes', as: :public_recipes

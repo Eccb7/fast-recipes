c262fa4b02b1a3933bfa5ee70ea48447a07f3d9e
Briton Otieno <ojwangbriton@gmail.com>
2024-02-10 22:23:35 +0300

    Add validations

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,17 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  validates :quantity, presence: true
+  validates :food_id, presence: true
+
+  attribute :food_name, :string
+  attribute :food_price, :decimal
+  attribute :quantity, :integer
+
+  attr_accessor :food_name, :food_price, :quantity
+
+  def self.permitted_attributes
+    %i[food_id quantity price]
+  end
 end
